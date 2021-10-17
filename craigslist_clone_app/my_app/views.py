7c6050b5a8eea9d14739cd5e8482bbbb1e1baf76
Seek4samurai <grvrawat999@gmail.com>
2021-10-17 08:40:02 +0530

    implementing Dynamic links

--- craigslist_clone_app/my_app/views.py
+++ craigslist_clone_app/my_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from bs4 import BeautifulSoup
+from typing import final
 import requests
+from requests.compat import quote_plus
+from bs4 import BeautifulSoup
+from django.shortcuts import render
 # Create your views here.
+
+BASE_CRAIGSLIST_URL = 'https://indore.craigslist.org/d/housing/search/hhh?query={}'
 
 
 def home(request):
@@ -10,5 +14,10 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    # print(final_url)
+    response = requests.get(final_url)
     stuff_for_frontend = {'search': search, }
+    data = response.text
+    # print(data)
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
