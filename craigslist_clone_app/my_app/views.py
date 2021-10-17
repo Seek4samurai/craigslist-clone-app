950075920f00df28961f9a2987d9aff76664ba7d
Seek4samurai <grvrawat999@gmail.com>
2021-10-17 09:54:57 +0530

    working html parser
    working web-scraping (using beautifulsoup)

--- craigslist_clone_app/my_app/views.py
+++ craigslist_clone_app/my_app/views.py
@@ -1,11 +1,12 @@
 from typing import final
 import requests
 from requests.compat import quote_plus
-from bs4 import BeautifulSoup
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+from . import models
 # Create your views here.
 
-BASE_CRAIGSLIST_URL = 'https://indore.craigslist.org/d/housing/search/hhh?query={}'
+BASE_CRAIGSLIST_URL = 'https://indore.craigslist.org/search/?query={}'
 
 
 def home(request):
@@ -14,10 +15,14 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
     final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
     # print(final_url)
     response = requests.get(final_url)
-    stuff_for_frontend = {'search': search, }
     data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_titles = soup.find_all('a', {'class': 'result-title'})
+    print(post_titles)
+    stuff_for_frontend = {'search': search, }
     # print(data)
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
