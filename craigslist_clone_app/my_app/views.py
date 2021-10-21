b9b8396fc84b4b8bc02e6fc9ff54cc5abfac1911
Seek4samurai <grvrawat999@gmail.com>
2021-10-21 19:49:29 +0530

    final commit

--- craigslist_clone_app/my_app/views.py
+++ craigslist_clone_app/my_app/views.py
@@ -1,12 +1,9 @@
-from typing import Text, final
 import requests
-from requests.compat import quote_plus
-from django.shortcuts import render
 from bs4 import BeautifulSoup
+from django.shortcuts import render
+from requests.compat import quote_plus
 from . import models
-# Create your views here.
 
-# change REGION here------------------------------------
 BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
@@ -25,32 +22,31 @@ def new_search(request):
 
     post_listings = soup.find_all('li', {'class': 'result-row'})
 
-    # post_titles = post_listings[0].find(class_='result-title').text
-    # post_url = post_listings[0].find('a').get('href')
-    # post_price = post_listings[0].find(class_='result-price')
-    # OPTIONAL PART FOR EMPTY PRICES----------------------------------
-    # if post_price == None:
-    #     post_price = 'Price Unspecified'
-
     final_postings = []
+
     for post in post_listings:
-        post_titles = post.find(class_='result-title').text
+        post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
+
         if post.find(class_='result-price'):
-            post_price = post.find(class_='result-price')
+            post_price = post.find(class_='result-price').text
         else:
             post_price = 'N/A'
 
-        if post.find(class_='result-image',).get('data-ids'):
+        if post.find(class_='result-image').get('data-ids'):
             post_image_id = post.find(
-                class_='result-image',).get('data-ids').split(',')[0].split(':')[1]
+                class_='result-image').get('data-ids').split(',')[0].split(':')[1]
             post_image_url = BASE_IMAGE_URL.format(post_image_id)
             print(post_image_url)
         else:
-            'https://craigslist.org/images/peace.jpg'
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
 
         final_postings.append(
-            (post_titles, post_url, post_price, post_image_url))
+            (post_title, post_url, post_price, post_image_url))
 
-    stuff_for_frontend = {'search': search, 'final_postings': final_postings}
+    stuff_for_frontend = {
+        'search': search,
+        'final_postings': final_postings,
+    }
+
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
